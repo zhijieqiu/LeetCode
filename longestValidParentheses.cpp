d8ae3ce88addb6c925878a856ac9a99693140fa4
zhijieqiu <1195116548@qq.com>
2014-09-15 22:03:50 +0800

    Update longestValidParentheses.cpp
    it is a program to find the longest valid parentheses from a string which only consists of '(' and ')'

--- longestValidParentheses.cpp
+++ longestValidParentheses.cpp
@@ -2,10 +2,12 @@ int longestValidParentheses(string s) {
 	int max = 0;
 	int curStart = 0;
 	int curValid = 0;
-	stack<pair<char, int>> st;
+	stack<pair<char, int>> st;//存储字符和它出现的位置
 
-	vector<pair<int, int>> validPairs;
+	vector<pair<int, int>> validPairs;//存储合理的括号对的左右位置，（（））会记录两次分别是(1,2) (0,3)
+	//因此需要在最后的扫描过程中判断
 	int i = 0;
+	//from line 11 to line 32, is to generate validPairs
 	while (i < s.length())
 	{
 		if (s[i] == '(')
@@ -32,22 +34,22 @@ int longestValidParentheses(string s) {
 		return 0;
 	int curLeft = validPairs[validPairs.size() - 1].first;
 	int right = validPairs[validPairs.size() - 1].second;
-	max = right-curLeft;
+	max = right-curLeft;//从第一个中获得合理的位置，以及记录他们的左右位置
 	for (int i = validPairs.size() - 2; i >= 0; i--)
 	{
-		if (validPairs[i].second == curLeft - 1)
+		if (validPairs[i].second == curLeft - 1)//如果该合理的右位置值和当前的范围的左值相同那么就合并
 		{
 			max = right - validPairs[i].first > max ? right - validPairs[i].first : max;
 			curLeft = validPairs[i].first;
 		}
-		else if (validPairs[i].first > curLeft&&validPairs[i].second < right)
+		else if (validPairs[i].first > curLeft&&validPairs[i].second < right)//如果已经在之前的范围中出现就不需要任何操作
 			;
-		else
+		else//否则就是已经出现了隔离段，要重新开始记录了，重新设定左右范围，并且计算它的合理情况
 		{
 			right = validPairs[i].second;
 			curLeft = validPairs[i].first;
 			max = right - curLeft > max ? right - curLeft : max;
 		}
 	}
-	return max+1;
+	return max+1;//remember to add 1 here 
 }
