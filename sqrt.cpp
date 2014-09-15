a94473dff512887009da7518508c697a2d02da18
zhijieqiu <1195116548@qq.com>
2014-09-15 16:14:03 +0800

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,20 +1,18 @@
 int sqrt(int x)
 {
-	
-	
-	long long left=0,right = x;
-	long long target = x;
-	while(left<right)
+    long long left=0,right = x;
+    long long target = x;
+    while(left<right)
+    {
+	long long t = (left+right)>>1;
+	if(t*t>target)
 	{
-		long long t = (left+right)>>1;
-		if(t*t>target)
-		{
-			right=t-1;
-		}
-		else if(t*t<target)
-			left=t+1;
-		else 
-			return t;
+		right=t-1;
 	}
-	return right*right>target?right-1:right;
+	else if(t*t<target)
+		left=t+1;
+	else 
+		return t;
+    }
+    return right*right>target?right-1:right;
 }
