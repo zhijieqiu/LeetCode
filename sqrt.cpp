ffd8c17f14808d70bcb95363f4507413e7791091
zhijieqiu <1195116548@qq.com>
2014-09-16 09:31:01 +0800

    Update sqrt.cpp

--- sqrt.cpp
+++ sqrt.cpp
@@ -1,10 +1,10 @@
 int sqrt(int x)
 {
-    long long left=0,right = x;
+    long long left=0,right = x;//此处将int转换为long long 是为了防止当x太大的时候溢出
     long long target = x;
     while(left<right)
     {
-	long long t = (left+right)>>1;
+	long long t = (left+right)>>1;//取中值
 	if(t*t>target)
 	{
 		right=t-1;
@@ -12,7 +12,8 @@ int sqrt(int x)
 	else if(t*t<target)
 		left=t+1;
 	else 
-		return t;
+		return t;//if t*t == target then t is the result
     }
-    return right*right>target?right-1:right;
+    return right*right>target?right-1:right;// when left>=right,then we can make a conclusion that the right value is the result
+    //or the (right-1) is the result
 }
