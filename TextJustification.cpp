d9dd864750b9c76a63b3df710e45ae4d38b0539f
zhijieqiu <1195116548@qq.com>
2014-09-17 15:56:49 +0800

    Update TextJustification.cpp

--- TextJustification.cpp
+++ TextJustification.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+      words:input strings
+      L:the max length of a line
+    */
     vector<string> fullJustify(vector<string> &words, int L) {
     	vector<string> ret;
     	int i = 0;
@@ -7,9 +11,9 @@ public:
     	{
     		string cur = "";
     		vector<string> tmp;
-    		int curWordsLength = words[i].length();
-    		tmp.push_back(words[i]);
-    		int gaps = 0;
+    		int curWordsLength = words[i].length();//to record the current min length of the current words
+    		tmp.push_back(words[i]);// push the first word to tmp,remember that tmp is empty at first
+    		int gaps = 0;// to record hom many gaps between words
     		i++;
     		while (i<words.size()&&curWordsLength + words[i].length() + 1 <= L)
     		{
@@ -19,21 +23,21 @@ public:
     			i++;
     		}
     		cur = tmp[0];
-    		if (gaps == 0)
+    		if (gaps == 0)//if just one word then append ' ' util it reach L
     		{
     			for (int j = cur.length() + 1; j <= L; j++)
     				cur += ' ';
     			ret.push_back(cur);
     			continue;
     		}
-    		if (i == words.size())
+    		if (i == words.size())// if the last word is finished,then we need to justification it well
     		{
-    			for (int j = 1; j <= gaps; j++)
+    			for (int j = 1; j <= gaps; j++)//the gap between words is just one ' '
     			{
     				cur += ' ';
     				cur += tmp[j];
     			}
-    			while (cur.length() < L)
+    			while (cur.length() < L)// make it reach the Length
     				cur += ' ';
     			ret.push_back(cur);
     			return ret;
