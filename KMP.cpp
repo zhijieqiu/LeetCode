9bf8a2af2cd9740715bc19e5274828ccc0dba9d6
zhijieqiu <1195116548@qq.com>
2014-09-17 20:04:41 +0800

    Create KMP.cpp

---
+++ KMP.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void getNext(char* pattern, int* next)
+{
+	
+	int i = 0, j = -1;
+	int len = strlen(pattern);
+	next[0] = -1;
+	while (i < len)
+	{
+		//当j==-1的时候是在计算next[0] ,以此类推
+		if (j == -1 || pattern[i] == pattern[j])
+		{
+			i++;
+			j++;
+			next[i] = j;
+		}
+		else
+			j = next[j];
+	}
+	for (int i = 0; i < len; i++)
+	{
+		cout << next[i] << " ";
+	}
+	cout << endl;
+}
+void find(char* pattern, char * str)
+{
+	int *next = new int[strlen(pattern)];
+	getNext(pattern, next);
+	int i = 0, j = 0, lenP = strlen(pattern), lenS = strlen(str);
+	while (true)
+	{
+		if (j==-1||pattern[j] == str[i])
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+			j = next[j];
+		}
+		if (j == lenP)
+		{
+			cout << "find at position " << i-lenP << endl;
+			j = next[j];
+		}
+		if (i == lenS)
+			break;
+	}
+}
+int main(void)
+{
+   char *str = "loveIlovelovelovehello,world,lovejackhellohelloohello";
+   char *pattern = "hello";
+   find(pattern, str);
+}
