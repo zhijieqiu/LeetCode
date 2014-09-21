e8260a692b67e6b25ad91e0a6d47a4b6c623b3f0
丘志杰 <1195116548@qq.com>
2014-09-21 12:01:25 +0800

    Update removeDumplicates.cpp

--- removeDumplicates.cpp
+++ removeDumplicates.cpp
@@ -1,3 +1,13 @@
+
+/*
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array A = [1,1,1,2,2,3],
+
+Your function should return length = 5, and A is now [1,1,2,2,3].
+*/
 class Solution {
 public:
    int removeDuplicates(int A[], int n) {
