99f5266e5ba20c2a9325c7ad85bcccea8e71e0a4
丘志杰 <1195116548@qq.com>
2014-09-25 14:20:58 +0800

    Update Subsets.cpp

--- Subsets.cpp
+++ Subsets.cpp
@@ -1,3 +1,11 @@
+
+/*
+method:subsets
+count:the subset element number
+fromIndex: the current begin index to search
+cur:the cur elements in current subset
+result:to hold the subsets
+*/
 void subsets(int count,int fromIndex, vector<int> cur, vector<vector<int> >& result,vector<int>& S)
 {
 	if (cur.size() == count)
@@ -9,18 +17,19 @@ void subsets(int count,int fromIndex, vector<int> cur, vector<vector<int> >& res
 	{
 		vector<int> tmp = cur;
 		tmp.push_back(S[i]);
-		if (tmp.size() + S.size() - i - 1 < count)
+		if (tmp.size() + S.size() - i - 1 < count)//if the current element add all the rest elements still small then count 
+		                                          //then there is no need to add go on the way
 			continue;
 		subsets(count, i + 1, tmp, result, S);
 	}
 }
 vector<vector<int> > subsets(vector<int> &S) {
 	vector<vector<int> > ret;
-	ret.push_back(vector<int>());
+	ret.push_back(vector<int>());//the empty subset must add here
 	sort(S.begin(), S.end());
 	for (int i = 1; i <= S.size(); i++)
 	{
-		subsets(i, 0, vector<int>(), ret, S);
+		subsets(i, 0, vector<int>(), ret, S);//add the subset which has i element
 	}
 	for (int i = 0; i < ret.size(); i++)
 	{
