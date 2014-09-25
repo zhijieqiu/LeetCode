26a0ef710b6b2956194899bcf9ec866a8e5124b9
丘志杰 <1195116548@qq.com>
2014-09-25 20:07:06 +0800

    Update recover_binary_search_tree.cpp

--- recover_binary_search_tree.cpp
+++ recover_binary_search_tree.cpp
@@ -7,7 +7,11 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
+ /*
+ 思路：使用中序遍历，一个中序遍历必然是一个有序的从小到大的序列
+ 交换其中任何两个节点必然导致这个有序序列发生变化，然后我们就需要找出这两个是的有序序列发生变化的节点就可以了
+ 使用beforeNode记录前一个节点
+ */
 class Solution {
 private:
     TreeNode *beforeNode=NULL,*node1=NULL,*node2=NULL;
