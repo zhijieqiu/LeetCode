cf0f40602e6902a6f2ce35edc995e8524f0b1286
丘志杰 <1195116548@qq.com>
2014-09-26 10:36:04 +0800

    Update isInterleave.cpp

--- isInterleave.cpp
+++ isInterleave.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    /*
+      思路类似使用：最长公共子序列的方法，使用动态数组dp[i][j]记录当前s1的前i个字符和s2的前j个字符
+      能够组合成s3的前i+j个字符
+    */
     bool isInterleave(string s1, string s2, string s3)
     {
     	int len1 = s1.length();
@@ -16,7 +20,7 @@ public:
     	{
     		for (int j = 0; j <= len2; j++)
     		{
-    			if (i == 0)
+    			if (i == 0)//判断s2与时s3的交集情况
     			{
     				if (j == 0)
     					continue;
@@ -32,6 +36,7 @@ public:
     					dp[i][j] = 1;
     				continue;
     			}
+    			//s3的最后一个字符要么是s1的最后一个字符，要么是s2的最后一个字符
     			if ((dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]) || (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]))
     				dp[i][j] = 1;
     			
