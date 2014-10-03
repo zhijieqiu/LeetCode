275f028f84f6b0b6177d35adc7d27f6c687e800d
丘志杰 <1195116548@qq.com>
2014-10-03 14:28:34 +0800

    Create canJump.cpp

---
+++ canJump.cpp
@@ -0,0 +1,19 @@
+/*
+思路：从开始往后面扫描，记录到目前为止能够跳的最远距离，如果下一个要准备的是的地方比最远的距离还远那么说明这次跳远也许是要失败了
+直接跳出循环判断判断最大值是否超过了终点
+*/
+class Solution {
+public:
+   bool canJump(int A[], int n) {
+    	int maxi = 0;
+    	for (int i = 0; i < n; i++)
+    	{
+    		maxi = maxi>(A[i] + i) ? maxi : (A[i] + i);
+    		if(maxi>=n-1)
+    		   return true;
+    		if (maxi < i + 1)
+    			break;
+    	}
+    	return maxi >= (n - 1);
+    }
+};
