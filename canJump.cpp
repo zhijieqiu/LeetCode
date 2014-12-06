ebef3a6c58ee287f2e01f45aae80a473154840ab
丘志杰 <1195116548@qq.com>
2014-12-06 21:06:19 +0800

    a test to che client
    a test to githut client

--- canJump.cpp
+++ canJump.cpp
@@ -2,7 +2,7 @@
 思路：从开始往后面扫描，记录到目前为止能够跳的最远距离，如果下一个要准备的是的地方比最远的距离还远那么说明这次跳远也许是要失败了
 直接跳出循环判断判断最大值是否超过了终点
 */
-class Solution {
+class Solution {// it is a test
 public:
    bool canJump(int A[], int n) {
     	int maxi = 0;
