b36388eaa1e2f747ad1e26917659f21a79a2fa6f
丘志杰 <1195116548@qq.com>
2015-03-26 18:27:57 +0800

    Update maxProduct.cpp
    这是一个简化版本的代码

--- maxProduct.cpp
+++ maxProduct.cpp
@@ -1,37 +1,32 @@
-int maxProduct(int A[], int n) {
-    int ret = 0;
-  	if(1==n)// 当只有一个元素的时候直接返回
-  	    return A[0];
-    int *max_p = new int[n];
-    int *max_n = new int[n];
-    max_p[0] = A[0]>0?A[0]:0;
-    max_n[0] = A[0]<0?A[0]:0;
-  	ret = max(ret,max_p[0]);
-    for(int i=1;i<n;i++)
-    {
-        if(A[i]>0)
-        {
-            int max1 = max_p[i-1]*A[i],max2=A[i];//如果A[i]大于0，那么下一个最大正数的来源就有可能有两种途径
-  		    int min1 = max_n[i-1]*A[i];
-  			max_p[i]=max(max1,max2);
-  			max_n[i]=min1;
-        }
-        else if(A[i]<0)
-        {
-            int max1 = max_n[i-1]*A[i];
-          	max_p[i] = max1;
-  			int min1 = A[i]*max_p[i-1],min2 = A[i];////如果A[i]小于0，那么下一个最大负数的来源就有可能有两种途径
-  			max_n[i]=min1<min2?min1:min2;
-        }
-        else
-    	{
-    		max_p[i]=0;
-    		max_n[i]=0;
-    	}
-    	if(max_p[i]>ret)
-    		ret = max_p[i];
-    	cout<<max_p[i]<<'\t'<<max_n[i]<<endl;
-    }
-    	
-    return ret;
-}
+
+class Solution {
+public:
+	int maxProduct(int A[], int n) {
+		int ret = 0;
+		if (1 == n)
+			return A[0];//check whether it is one element
+		// 2 3 -2 4
+		int maxP = 0, maxN = 0;
+		for (int i = 0; i < n; i++)
+		{
+			if (A[i]>0)
+			{
+				maxP = max(maxP*A[i],A[i]);//maxp has only two ways to come here
+				maxN = min(maxN*A[i], 0);//maxn can only be two ways to come here
+			}
+			else if (A[i] < 0)
+			{
+				int t = maxP;
+				maxP = max(maxN*A[i], 0);
+				maxN = min(t*A[i],A[i]);
+			}
+			else
+			{
+				maxP = 0;
+				maxN = 0;
+			}
+			ret = max(ret,maxP);
+		}
+		return ret;
+	}
+};
